f0ace685a96146169dee534c13b87cee13151024
Carlos Jaramillo <cejaramillof@mercadolibre.com.co>
2022-02-20 08:53:01 -0500

    feat: add docusaurus scaffolding, and github action to deploy pages

---
+++ docusaurus.config.js
@@ -0,0 +1,65 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Web Development',
+  tagline: 'Guide',
+  url: 'https://cejaramillof.github.io',
+  baseUrl: '/web/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'cejaramillof',
+  projectName: 'web.github.io',
+  trailingSlash: false,
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        blog: false,
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Web Development',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            href: 'https://github.com/cejaramillof/web',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        copyright: 'by {cejaramillof}',
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
